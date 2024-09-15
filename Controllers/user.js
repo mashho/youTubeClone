00a822bce51206477b8b5901983b05c6e0fd4f75
Mashhood Ahmad Danish <46836316+mashho@users.noreply.github.com>
2024-09-15 14:29:24 +0530

    Update user.js

--- Controllers/user.js
+++ Controllers/user.js
@@ -15,14 +15,7 @@ exports.signUp = async(req,res)=>{
         const { channelName, userName, about, profilePic, password } = req.body;
         const isExist = await User.findOne({ userName });
         
-        if(isExist){
-            res.status(400).json({ error: "Username Already Exist Please try with other username" });
-        }else{
-            let updatedPass = await bcrypt.hash(password, 10);
-            const user = new User({ channelName, userName, about, profilePic, password: updatedPass });
-            await user.save();
-            res.status(201).json({ message: 'User registered successfully', success: "yes",data:user });
-        }
+        // please watch the video for the code
         
     } catch (error){
         res.status(500).json({ error: 'Server error' });
@@ -33,17 +26,7 @@ exports.signIn = async (req,res)=>{
     try{
         const { userName, password } = req.body;
         const user = await User.findOne({ userName });
-        if(user && await bcrypt.compare(password, user.password)){
-            
-            const token = jwt.sign({ userId: user._id }, 'Its_My_Secret_Key');
-            
-            res.cookie('token', token,cookieOptions);
-           
-            res.json({ message: 'Logged in successfully', success: "true",token,user});
-
-        }else{
-            res.status(400).json({ error: 'Invalid credentials' });
-        }
+        // please watch the video for the code
     } catch (errorMsg){
         res.status(500).json({ error: 'Server error' });
     }
